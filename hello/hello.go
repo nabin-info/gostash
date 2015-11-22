ad997f1115e34c6b3e52760aab7eb1dd343f437e
the.nabin.info <ryan@the.nabin.info>
2015-11-22 02:42:42 -0800

    modified hello to use stringutil package

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+import "github.com/nabin-info/gostash/stringutil"
+
 func main() {
 	fmt.Printf("Hello, World\n")
+	fmt.Printf(stringutil.Reverse("Hello, World\n"))
+
 }
